3da7f56e8551b86cd059f30dd820b80a9488ba65
Yves Xie <yxi389@gmail.com>
2013-06-29 20:52:54 -0500

    new utility class

---
+++ src/main/scala/com/mycode/Utilities.scala
@@ -0,0 +1,12 @@
+package com.mycode
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yves
+ * Date: 6/29/13
+ * Time: 8:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Utilities {
+
+}
