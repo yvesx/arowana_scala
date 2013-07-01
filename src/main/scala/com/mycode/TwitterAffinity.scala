eb94ca3ad10f88be51ec7f020cca2e2418218772
Yves Xie <yxi389@gmail.com>
2013-07-01 16:33:18 -0500

    integrate user level caching

--- src/main/scala/com/mycode/TwitterAffinity.scala
+++ src/main/scala/com/mycode/TwitterAffinity.scala
@@ -19,37 +19,43 @@ class TwitterAffinity () extends CacheableQuery {
     partial_fields = "{\"partial1\": { \"include\": \"user.id\"} }",
     size = "1000"
   )
-  def affQuery: ElasticQuery = new ElasticQuery(
+  def affQuery(userIDs: String): ElasticQuery = new ElasticQuery(
     host = userQuery.host,
     index = userQuery.index,
     mapping = userQuery.mapping,
-    must = """[ { "terms": { "for_hashtag.user.id":[ %s ]} } ]""".format(parseUsers),
+    must = """[ { "terms": { "for_hashtag.user.id":[ %s ]} } ]""".format(userIDs),
     mustNot = """{ "missing": { "field": "entities.hashtags.text", "existence": true, "null_value": true } }""",
     partial_fields = """{"partial1": { "include": [ "user.id", "entities.hashtags.text"]}}""",
     size = userQuery.size
   )
-  def parseUsers: String = {
+  def parseUsers: Map[String,Seq[String]] = {
     val records = userQuery.parseElasticHits()
-    var userIDs: Vector[String] = Vector[String]()
+    var userIDs: List[String] = List[String]()
     for (record <- records) {
       val userID:String = record.get("fields").get.asInstanceOf[Map[String,Any]]
         .get("partial1").get.asInstanceOf[Map[String,Any]]
         .get("user").get.asInstanceOf[Map[String,Double]]
         .get("id").get.toString
 
-      userIDs = userIDs:+ userID
+      userIDs = userID +: userIDs
     }
     //search cache first
     val cache: Seq[(Any, Option[String])] = mgetZip(userIDs:_*)
-    ZipHits(cache)
-    ZipMiss(cache)
-    return com.mycode.Utilities.concat(userIDs,",")
+    val tags = ZipHits(cache).map(_._2.get)
+    val missedUsers = ZipMiss(cache).map(_._1.toString)
+    return Map("tagsFromCache"->tags,"missedUsers" -> missedUsers)
+    //return com.mycode.Utilities.concat(missedUsers,",")
   }
 
-  def parseAffinities: Vector[String] = {
-    val records = affQuery.parseElasticHits()
-    var userHashtags: Vector[String] = Vector[String]()
+  def findAffinities: List[String] = {
+    // try affinity cache first
+
+    //
+    val usersMixed = parseUsers
+    val records = affQuery(com.mycode.Utilities.concat(usersMixed.get("missedUsers").get,",")).parseElasticHits()
+    var userHashtags: List = com.mycode.Utilities.flatten(usersMixed.get("tagsFromCache").get.map(_.split(",").asInstanceOf[List[String]]).asInstanceOf[List])
     var KeyValuePairs: Seq[(String,String)] = Seq[(String,String)]()
+
     for (record <- records) {
       val tags:List[String] = record.get("fields").get.asInstanceOf[Map[String,Any]]
         .get("partial1").get.asInstanceOf[Map[String,Any]]
@@ -62,18 +68,18 @@ class TwitterAffinity () extends CacheableQuery {
         .get("id").get.asInstanceOf[Double].toString
       //userIDs = userIDs :+ userID
       val tagsString:String = com.mycode.Utilities.concat(tags,",")
-      KeyValuePairs = KeyValuePairs :+ (userID,tagsString)
-      for (tag <- tags) {
-        userHashtags = userHashtags :+ tag
-      }
+      KeyValuePairs = (userID,tagsString) +: KeyValuePairs
+
+      userHashtags = tags ++ userHashtags
+
     }
     //Store to cache, if parseAffinities is called. mset is called every time
     mset(KeyValuePairs:_*)
-    return userHashtags
+    return userHashtags.asInstanceOf[List[String]]
   }
 
   def mapReduceAffinities: Seq[(String, Int)] =
-    parseAffinities.map(_.toLowerCase.trim).groupBy(identity).mapValues(_.size).toSeq.sortBy(_._2)
+    findAffinities.map(_.toLowerCase.trim).groupBy(identity).mapValues(_.size).toSeq.sortBy(_._2)
 
   def printAffinities: Unit = com.mycode.Utilities.printSeq(mapReduceAffinities)
 }
