a24704d07821668aa777eec3440fb0316770d78e
Yves Xie <yxi389@gmail.com>
2013-07-01 14:32:05 -0500

    refactor in twitterAffinity class

--- src/main/scala/com/mycode/TwitterAffinity.scala
+++ src/main/scala/com/mycode/TwitterAffinity.scala
@@ -1,5 +1,5 @@
 package com.mycode
-
+import com.redis._
 /**
  * Created with IntelliJ IDEA.
  * User: yves
@@ -8,7 +8,9 @@ package com.mycode
  * To change this template use File | Settings | File Templates.
  */
 class TwitterAffinity () extends CacheableQuery {
-  def userQuery: ElasticQuery = new ElasticQuery(
+  val cacheHost: String = "localhost"
+  val cachePort: Int = 6379
+  val userQuery: ElasticQuery = new ElasticQuery(
     host = "http://localhost:8080",
     index = "twitter_mentioned_hash",
     mapping = "for_hashtag",
@@ -17,7 +19,16 @@ class TwitterAffinity () extends CacheableQuery {
     partial_fields = "{\"partial1\": { \"include\": \"user.id\"} }",
     size = "1000"
   )
-  def parseUsers(): String = {
+  def affQuery: ElasticQuery = new ElasticQuery(
+    host = userQuery.host,
+    index = userQuery.index,
+    mapping = userQuery.mapping,
+    must = """[ { "terms": { "for_hashtag.user.id":[ %s ]} } ]""".format(parseUsers),
+    mustNot = """{ "missing": { "field": "entities.hashtags.text", "existence": true, "null_value": true } }""",
+    partial_fields = """{"partial1": { "include": [ "user.id", "entities.hashtags.text"]}}""",
+    size = userQuery.size
+  )
+  def parseUsers: String = {
     val records = userQuery.parseElasticHits()
     var userIDs: Vector[String] = Vector[String]()
     for (record <- records) {
@@ -31,34 +42,34 @@ class TwitterAffinity () extends CacheableQuery {
     return com.mycode.Utilities.concat(userIDs,",")
   }
 
-  def affQuery: ElasticQuery = new ElasticQuery(
-    host = userQuery.host,
-    index = userQuery.index,
-    mapping = userQuery.mapping,
-    must = """[ { "terms": { "for_hashtag.user.id":[ %s ]} } ]""".format(parseUsers()),
-    mustNot = """{ "missing": { "field": "entities.hashtags.text", "existence": true, "null_value": true } }""",
-    partial_fields = """{"partial1": { "include": [ "user.id", "entities.hashtags.text"]}}""",
-    size = userQuery.size
-  )
-
-  def parseAffinities(): Vector[String] = {
+  def parseAffinities: Vector[String] = {
     val records = affQuery.parseElasticHits()
     var userHashtags: Vector[String] = Vector[String]()
+    var KeyValuePairs: Seq[(String,String)] = Seq[(String,String)]()
     for (record <- records) {
-      val tags:List[Map[String,String]] = record.get("fields").get.asInstanceOf[Map[String,Any]]
+      val tags:List[String] = record.get("fields").get.asInstanceOf[Map[String,Any]]
         .get("partial1").get.asInstanceOf[Map[String,Any]]
         .get("entities").get.asInstanceOf[Map[String,Any]]
-        .get("hashtags").get.asInstanceOf[List[Map[String,String]]]
+        .get("hashtags").get.asInstanceOf[List[Map[String,String]]].map(_.get("text").get)
 
+      val userID:String = record.get("fields").get.asInstanceOf[Map[String,Any]]
+        .get("partial1").get.asInstanceOf[Map[String,Any]]
+        .get("user").get.asInstanceOf[Map[String,Any]]
+        .get("id").get.asInstanceOf[Double].toString
+      //userIDs = userIDs :+ userID
+      val tagsString:String = com.mycode.Utilities.concat(tags,",")
+      KeyValuePairs = KeyValuePairs :+ (userID,tagsString)
       for (tag <- tags) {
-        userHashtags = userHashtags :+ tag.get("text").get
+        userHashtags = userHashtags :+ tag
       }
     }
+    //Store to cache, if parseAffinities is called. mset is called every time
+    mset(KeyValuePairs:_*)
     return userHashtags
   }
 
   def mapReduceAffinities: Seq[(String, Int)] =
-    parseAffinities().map(_.toLowerCase.trim).groupBy(identity).mapValues(_.size).toSeq.sortBy(_._2)
+    parseAffinities.map(_.toLowerCase.trim).groupBy(identity).mapValues(_.size).toSeq.sortBy(_._2)
 
   def printAffinities: Unit = com.mycode.Utilities.printSeq(mapReduceAffinities)
 }
