a0b8a766d01e1fd38d0d21da85ef7ab96c887c1a
Yves Xie <yxi389@gmail.com>
2013-07-01 16:33:26 -0500

    add a utility

--- src/main/scala/com/mycode/Utilities.scala
+++ src/main/scala/com/mycode/Utilities.scala
@@ -14,4 +14,11 @@ object Utilities {
   def concat(strings: Seq[String],delim: String): String = (strings filter {
     _.nonEmpty
   }).mkString(delim)
+  def flatten[T](list: List[T]): List[T] = list match {
+    case Nil => Nil
+    case head :: tail => (head match {
+      case l: List[T] => flatten(l)
+      case i => List(i)
+    }) ::: flatten(tail)
+  }
 }
