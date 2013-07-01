135d5d3e1193f7ae46060beaf49fa4ccdb733aaa
Yves Xie <yxi389@gmail.com>
2013-07-01 14:31:51 -0500

    cacheable class

--- src/main/scala/com/mycode/CacheableQuery.scala
+++ src/main/scala/com/mycode/CacheableQuery.scala
@@ -1,5 +1,5 @@
 package com.mycode
-
+import com.redis._
 /**
  * Created with IntelliJ IDEA.
  * User: yves
@@ -8,5 +8,17 @@ package com.mycode
  * To change this template use File | Settings | File Templates.
  */
 trait CacheableQuery {
-
+   var cacheHost: String
+   var cachePort: Int
+   val r:RedisClient = new RedisClient(cacheHost, cachePort)
+   def mget(keys:Any*): List[Option[String]] = {
+     r.mget(Nil,keys:_*).get
+   }
+  def mset(keyValuePairs:(Any,Any)*): Boolean = {
+    r.mset(keyValuePairs:_*)
+  }
+  def getCacheHits(keys:Any*): Double = {
+    val lst = mget(keys)
+    lst.count(_.isDefined)/lst.size
+  }
 }
