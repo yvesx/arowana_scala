a2787493289a12d165c16fda4becd3cb06707a5a
Yves Xie <yxi389@gmail.com>
2013-07-01 15:39:25 -0500

    design more cacheable queries

--- src/main/scala/com/mycode/CacheableQuery.scala
+++ src/main/scala/com/mycode/CacheableQuery.scala
@@ -14,11 +14,26 @@ trait CacheableQuery {
    def mget(keys:Any*): List[Option[String]] = {
      r.mget(Nil,keys:_*).get
    }
+  def mgetZip(keys:Any*): Seq[(Any,Option[String])] = {
+    return keys zip mget(keys)
+  }
+  def mgetHits(keys:Any*): List[String] = {
+    mget(keys).filter(_.isDefined).map(_.get)
+  }
+  def mgetMiss(keys:Any*): List[String] = {
+    mget(keys).filterNot(_.isDefined).map(_.get)
+  }
+  def ZipHits(cache: Seq[(Any,Option[String])]): Seq[(Any,Option[String])] = {
+    cache.filter(_._2.isDefined)
+  }
+  def ZipMiss(cache: Seq[(Any,Option[String])]): Seq[(Any,Option[String])] = {
+    cache.filterNot(_._2.isDefined)
+  }
   def mset(keyValuePairs:(Any,Any)*): Boolean = {
     r.mset(keyValuePairs:_*)
   }
   def getCacheHits(keys:Any*): Double = {
-    val lst = mget(keys)
+    val lst = r.mget(Nil,keys:_*).get
     lst.count(_.isDefined)/lst.size
   }
 }
