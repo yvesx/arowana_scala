91665397b1c175eb9465de5de91ad41b824d28cb
Yves Xie <yxi389@gmail.com>
2013-07-01 19:20:25 -0500

    simplify some cache methods

--- src/main/scala/com/mycode/CacheableQuery.scala
+++ src/main/scala/com/mycode/CacheableQuery.scala
@@ -8,20 +8,16 @@ import com.redis._
  * To change this template use File | Settings | File Templates.
  */
 trait CacheableQuery {
-   var cacheHost: String
-   var cachePort: Int
-   val r:RedisClient = new RedisClient(cacheHost, cachePort)
-   def mget(keys:Any*): List[Option[String]] = {
-     r.mget(Nil,keys:_*).get
-   }
-  def mgetZip(keys:Any*): Seq[(Any,Option[String])] = {
-    return keys zip mget(keys)
+   val r:RedisClient
+
+  def mgetZip(keys:List[String],mgetResults:List[Option[String]]): Seq[(Any,Option[String])] = {
+    return keys zip mgetResults
   }
-  def mgetHits(keys:Any*): List[String] = {
-    mget(keys).filter(_.isDefined).map(_.get)
+  def mgetHits(mgetResults:List[Option[String]]): List[String] = {
+    mgetResults.filter(_.isDefined).map(_.get)
   }
-  def mgetMiss(keys:Any*): List[String] = {
-    mget(keys).filterNot(_.isDefined).map(_.get)
+  def mgetMiss(mgetResults:List[Option[String]]): List[String] = {
+    mgetResults.filterNot(_.isDefined).map(_.get)
   }
   def ZipHits(cache: Seq[(Any,Option[String])]): Seq[(Any,Option[String])] = {
     cache.filter(_._2.isDefined)
@@ -29,9 +25,7 @@ trait CacheableQuery {
   def ZipMiss(cache: Seq[(Any,Option[String])]): Seq[(Any,Option[String])] = {
     cache.filterNot(_._2.isDefined)
   }
-  def mset(keyValuePairs:(Any,Any)*): Boolean = {
-    r.mset(keyValuePairs:_*)
-  }
+
   def getCacheHits(keys:Any*): Double = {
     val lst = r.mget(Nil,keys:_*).get
     lst.count(_.isDefined)/lst.size
